9186754a01eb75a84912f7a92f9328c8340281b0
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2026-01-10 20:32:36 +0900

    Flash メッセージが表示されるようにした追加

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -4,7 +4,18 @@ class HelloController < ApplicationController
   def index
     @header = "layout sample"
     @fooder = "copyight mackey 2026."
-    @title = "New Layout"
-    @msg = "this is sample page!"
+    @title = "Session sample"
+
+    if request.post?
+      if params[:name].blank?
+        flash[:notice] = "Please input your name!"
+      else
+        @msg = "Hello #{params[:name]}!"
+        flash[:notice] = "登録が完了しました！"
+      end
+      redirect_to action: "index", title: @title
+    else
+      @msg = params[:msg]? params[:msg] : "Sample flash."
+    end
   end
 end
