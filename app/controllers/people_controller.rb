e071fe5b24034425fa82e3991d36867081106870
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2026-01-13 22:44:11 +0900

    people 詳細機能追加

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,4 +3,9 @@ class PeopleController < ApplicationController
     @msg = 'Person data.'
     @data = Person.all
   end
+
+  def show
+    @msg = "Index data."
+    @data = Person.find(params[:id])
+  end
 end
