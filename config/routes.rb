467635747d691e0bf683aecafec183809fe86b54
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2026-01-14 08:39:03 +0900

    people 更新機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "people/add"
   post "people/add", to: "people#create"
   get "people/:id", to: "people#show"
+  get "people/edit/:id", to: "people#edit"
+  patch "people/edit/:id", to: "people#update"
+
   get "msgboard", to: "msgboard#index"
   post "msgboard", to: "msgboard#index"
   get "msgborad/index"
